4dd73a209e3b9ee7e7a264c126db371006ca8f58
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-09-26 22:59:39 +0900

    1단계 완

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-// import MovieCard from './components/MovieCard'
-import MovieDetail from './components/MovieDetail'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+import MovieDetail from "./components/MovieDetail";
 
 function App() {
-
   return (
-    <>
-      {/* <MovieCard/> */}
-      <MovieDetail/>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<MovieCard />} />
+      <Route path="/details" element={<MovieDetail />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import movieListDatas from "../../data/movieListData.json";
 import "./MovieCard.css";
 const movieListData = movieListDatas.results;
-const baseUrl = "https://image.tmdb.org/t/p/w500" 
+const baseUrl = "https://image.tmdb.org/t/p/w500";
 
-export const MovieCard = () => {
+export default function MovieCard() {
+  const navigate = useNavigate();
+  const handleCardClick = () => {
+    navigate("/details");
+  };
   return (
-    <div className="card-container">{movieListData.map((item) => (
-        <div
-          className="card-wrap"
-          key={item.id}
-        >
-            <img className="poster"
-            src={baseUrl+item.poster_path}
+    <div className="card-container">
+      {movieListData.map((item) => (
+        <div className="card-wrap" key={item.id} onClick={handleCardClick}>
+          <img
+            className="poster"
+            src={baseUrl + item.poster_path}
             alt={item.title}
-            />
-            <div className="text-wrap">
-                <h1 className="title">{item.title}</h1>
-                <h2 className="vote-average">평점: {item.vote_average}</h2>
-            </div>
-        </div>))}
+          />
+          <div className="text-wrap">
+            <h1 className="title">{item.title}</h1>
+            <h2 className="vote-average">평점: {item.vote_average}</h2>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
