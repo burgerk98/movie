3bd9248bc1c932c4d8fd3cd60b94d2903e659e11
강계령 <153696250+burgerk98@users.noreply.github.com>
2024-09-26 13:29:24 +0900

    1일차-1

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+import movieListDatas from "../../data/movieListData.json";
+import "./MovieCard.css";
+const movieListData = movieListDatas.results;
+const baseUrl = "https://image.tmdb.org/t/p/w500" 
+
+export const MovieCard = () => {
+  return (
+    <div className="card-container">{movieListData.map((item) => (
+        <div
+          className="card-wrap"
+          key={item.id}
+        >
+            <img className="poster"
+            src={baseUrl+item.poster_path}/>
+            <div className="text-wrap">
+                <h1 className="title">{item.title}</h1>
+                <h2 className="vote-average">평점: {item.vote_average}</h2>
+            </div>
+        </div>))}
+    </div>
+  )
+}
